ec7cc639c2c1370518b76bcdf415a75e556b4dab
Amy Nicholson <missamynicholson@gmail.com>
2016-05-11 17:07:08 +0100

    Refactor

--- thermostatJQuery.js
+++ thermostatJQuery.js
@@ -1,72 +1,71 @@
 $( document ).ready(function() {
+
   thermostat = new Thermostat();
-      $( "#increase" ).click(function( event ) {
-          thermostat.increase(1);
-          $("#temperature").text(thermostat.value());
-          $("body").attr("style", function(){
-            return "background-color:" + thermostat.displayColour() + ";"
-          });
-      });
-      $( "#decrease" ).click(function( event ) {
-          thermostat.decrease(1);
-          $("#temperature").text(thermostat.value());
-          $("body").attr("style", function(){
-            return "background-color:" + thermostat.displayColour() + ";"
-          });
-      });
-      $( "#reset" ).click(function( event ) {
-          thermostat.reset();
-          $("#temperature").text(thermostat.value());
-          $("body").attr("style", function(){
-            return "background-color:" + thermostat.displayColour() + ";"
-          });
-      });
 
-      $( "#powerSaveSwitch" ).click(function( event ) {
-          thermostat.powerSaveSwitch();
-          $("#powerModeStatus").text(function() {
-            if (thermostat.powerSaveStatus)
-              return "On"
-            else
-            return "Off"
-          })
-      });
+  $( "#increase" ).click(function( event ) {
+      thermostat.increase(1);
+      updateTemperatureDisplay()
+      updateDisplayColour ()
+  });
 
-      $("#temperature").text(thermostat.value());
+  $( "#decrease" ).click(function( event ) {
+      thermostat.decrease(1);
+      updateTemperatureDisplay()
+      updateDisplayColour ()
+  });
 
-      $("#powerModeStatus").text(function() {
-        if (thermostat.powerSaveStatus)
-          return "On"
-        else
-        return "Off"
-      })
+  $( "#reset" ).click(function( event ) {
+      thermostat.reset();
+      updateTemperatureDisplay()
+      updateDisplayColour ()
+  });
 
-      $("body").attr("style", function(){
-        return "background-color:" + thermostat.displayColour() + ";"
-      });
+  $( "#powerSaveSwitch" ).click(function( event ) {
+      thermostat.powerSaveSwitch();
+      updatePowerModeStatusDisplay()
+  });
 
-      // var cities = document.getElementById("cities"),
-      //   city = cities.options[cities.selectedIndex].value;
+  updateTemperatureDisplay()
+  updatePowerModeStatusDisplay()
+  updateDisplayColour ()
+  getWeather("2643743")
 
-      var select = document.forms[0].cities;
-      select.onchange = function(){
-        var value = select.options[select.selectedIndex].value;
-        getWeather(value)
-      }
+  function updateTemperatureDisplay() {
+    $("#temperature").text(thermostat.value());
+  }
 
-      function getWeather(city) {
-        var url = 'http://api.openweathermap.org/data/2.5/weather?id=' + city + '&APPID=0ea98111060a61f6a3408109332873c0&units=metric'; 
-        console.log(url);
-        $.ajax({
-        json: "callback",
-        url: url,
-        dataType: "json",
-        success: function(response) {
-          $("#weatherTemp").text(function() {
-            console.log(response.main.temp);
-            return response.main.temp + " C";
-          });
-        }
+  function updateDisplayColour () {
+    $("body").attr("style", function(){
+      return "background-color:" + thermostat.displayColour() + ";"
+    });
+  }
+
+  function updatePowerModeStatusDisplay() {
+    $("#powerModeStatus").text(function() {
+      if (thermostat.powerSaveStatus)
+        return "On"
+      else
+      return "Off"
+    })
+  }
+
+  var select = document.forms[0].cities;
+  select.onchange = function(){
+    var value = select.options[select.selectedIndex].value;
+    getWeather(value)
+  }
+
+  function getWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?id=' + city + '&APPID=0ea98111060a61f6a3408109332873c0&units=metric';
+    $.ajax({
+      json: "callback",
+      url: url,
+      dataType: "json",
+      success: function(response) {
+        $("#weatherTemp").text(function() {
+          return response.main.temp + " C";
         });
       }
+    });
+  }
 });
